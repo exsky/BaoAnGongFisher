d7a9e335cb48d9c8f863c0556d905bf9b2c9a656
exsky <sky@nipapa.tw>
2022-09-13 11:40:10 +0800

    :wrench: extract fish card view, update fish data

--- BaoAnGongFisher/ToxicFish.swift
+++ BaoAnGongFisher/ToxicFish.swift
@@ -18,18 +18,8 @@ var toxicFishesData = [
     FishRect(fishName: "赤魟", fishCategory: "魟", fishNickName: ["赤土魟", "紅魴魚", "牛尾魴"]),
     FishRect(fishName: "黃魟", fishCategory: "魟", fishNickName: ["笨氏土魟", "黃魴", "紅魴"]),
     FishRect(fishName: "石狗公", fishCategory: "虎", fishNickName: ["石頭魚", "獅甕", "紅鱠仔"]),
-    FishRect(fishName: "花身鯻", fishCategory: "斑午", fishNickName: ["花身雞魚", "花身仔", "雞仔魚", "斑午"])
+    FishRect(fishName: "花身鯻", fishCategory: "斑午", fishNickName: ["花身雞魚", "花身仔", "雞仔魚", "斑午"]),
+    FishRect(fishName: "線紋鰻鯰", fishCategory: "沙毛", fishNickName: ["鰻鯰", "沙毛", "海土虱", "斜門"])
 ]
 
-var fishCates = ["魟", "虎", "斑午"]
-
-// 掃描所有魚資料，整理有哪些分類
-//func allFishCates(inputArray:[FishRect]) -> Array<Any> {
-//    var fishCates: [String] = []
-//    for toxicfish in toxicFishesData {
-//        if fishCates.contains(toxicfish.fishCategory) == false {
-//            fishCates.append(toxicfish.fishCategory)
-//        }
-//    }
-//    return fishCates
-//}
+var fishCates = ["魟", "虎", "沙毛", "斑午"]
--- BaoAnGongFisher/ToxicFishListView.swift
+++ BaoAnGongFisher/ToxicFishListView.swift
@@ -13,42 +13,21 @@ struct ToxicFishListView: View {
     
     var body: some View {
         NavigationView {
-            VStack{
+            VStack {
                 ForEach(fishCates, id: \.self) { fishCate in
                     VStack {
                         NavigationView {
                             List(toxicfishes, id: \.self) { toxicfish in    // 每一種魚
                                 // Fish Rectangle
                                 if toxicfish.fishCategory == fishCate {
-                                    HStack {    // 有毒的魚，會以水平方式堆疊其他元件
-                                        // 最左邊放一張圖片
-                                        Image(toxicfish.imageName)
-                                            .resizable()
-                                            .frame(width: 40, height: 40)
-                                            .cornerRadius(4)
-                                        // 放一個垂直堆疊
-                                        VStack {
-                                            // 魚的名字
-                                            HStack {
-                                                Text(toxicfish.fishName)
-                                                    .font(.headline)
-                                                    .fontWeight(.bold)
-                                                    .frame(alignment: .leading)
-                                                Spacer()
-                                            }
-                                            // 魚的俗名，把 [String] join 成一個大的字串
-                                            HStack {
-                                                Text(toxicfish.fishNickName.joined(separator: "、 "))
-                                                    .frame(width: 200, alignment: .leading)
-                                                    .background(Color(hue: 0.279, saturation: 1.0, brightness: 1.0, opacity: 0.21))
-                                                Spacer()
-                                            }
-                                        }
-                                    }
+                                    FishCardView(
+                                        imgName: toxicfish.imageName,
+                                        fishName: toxicfish.fishName,
+                                        fishNickName: toxicfish.fishNickName)
                                 }   // end of 每一種魚
                             }
                             .listRowSeparator(.hidden)
-                            .navigationTitle(fishCate)  // 危險魚類圖鑑
+                            .navigationTitle(fishCate)
                         }
                     }   // end of 每一分類魚
                 }
@@ -63,5 +42,50 @@ struct ToxicFishListView_Previews: PreviewProvider {
         ToxicFishListView()
         ToxicFishListView()
             .preferredColorScheme(.dark)
+    }
+}
+
+// 魚卡
+struct FishCardView: View {
+    
+    // 先定義好要接收的參數名稱與類型
+    var imgName: String
+    var fishName: String
+    var fishNickName: [String]
+    
+    var body: some View {
+        HStack {    // 有毒的魚，會以水平方式堆疊其他元件
+            // 最左邊放一張圖片
+            Image(imgName)
+                .resizable()
+                .frame(width: 40, height: 40)
+                .cornerRadius(4)
+            // 放一個垂直堆疊
+            VStack {
+                // 魚的名字
+                HStack {
+                    Text(fishName)
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .frame(alignment: .leading)
+                    Spacer()
+                }
+                // 魚的俗名，把 [String] join 成一個大的字串
+                HStack {
+                    Text(fishNickName.joined(separator: "、 "))
+                        .frame(width: 200, alignment: .leading)
+                        .background(Color(hue: 0.279, saturation: 1.0, brightness: 1.0, opacity: 0.21))
+                    Spacer()
+                }
+            }
+            Button(action: {
+                if let yourURL = URL(string: "www.youtube.com") {
+                    UIApplication.shared.open(yourURL, options: [:], completionHandler: nil)
+                }
+
+            }) {
+               Text("Go to youtube")
+            }
+        }
     }
 }
