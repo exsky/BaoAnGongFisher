43335ff2a824d63af87c4991af85423a8efaee8c
exsky <sky@nipapa.tw>
2022-09-21 10:17:54 +0800

    :wrench: refactor: split secret location data, gen annotation with iteration

--- BaoAnGongFisher/FishingLocationView.swift
+++ BaoAnGongFisher/FishingLocationView.swift
@@ -10,6 +10,7 @@ import MapKit
 import CoreLocationUI
 
 struct FishingLocationView: View {
+
     var body: some View {
         VStack {
             NavigationView {
@@ -21,12 +22,67 @@ struct FishingLocationView: View {
 }
 
 struct MapView: View {
+    // 用來記錄長按手勢是否被觸發
+    @State private var isLongPressed = false
     @StateObject private var viewModel = FishingLocationModel()
+    @State private var SecretLocations = SecretLocationsData
+
     var body: some View {
         ZStack(alignment: .topTrailing) {
-            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+            Map(coordinateRegion: $viewModel.region,
+                showsUserLocation: true,
+                annotationItems: SecretLocations) { item in
+                    // MapMarker(coordinate: item.coordinate, tint: .red)
+                    MapAnnotation(coordinate: item.coordinate) {
+                        Text(item.name)
+                            .fontWeight(.light)
+                            .font(.caption2)
+                        switch item.rank {
+                        case 1:
+                            Image(systemName: "hand.thumbsdown.circle.fill")
+                                .foregroundColor(.red)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        case 2:
+                            Image(systemName: "tortoise.fill")
+                                .foregroundColor(.orange)
+                        case 3:
+                            Image(systemName: "questionmark.circle.fill")
+                                .foregroundColor(.gray)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        case 4:
+                            Image(systemName: "face.smiling.fill")
+                                .foregroundColor(.cyan)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        case 5:
+                            Image(systemName: "hand.thumbsup.circle.fill")
+                                .foregroundColor(.green)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        default:
+                            Image(systemName: "questionmark.circle.fill")
+                                .foregroundColor(.gray)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        }
+                    }
+                }
                 .edgesIgnoringSafeArea(.all)
-                .tint(.green)
+                .gesture(DragGesture())
+                .onLongPressGesture {
+                    isLongPressed.toggle()
+                }.actionSheet(isPresented: $isLongPressed) {
+                    ActionSheet(title: Text("新增私房釣點嗎？"),
+                                message: nil,
+                                buttons: [
+                                    .default(Text("新增釣點")) {
+                                        // Yes
+                                    },
+                                    .cancel()
+                                ])
+                }
             LocationButton(.currentLocation) {
                 viewModel.requestAllowOnceLocationPermission()
             }
@@ -51,8 +107,8 @@ struct FishingLocationView_Previews: PreviewProvider {
 
 final class FishingLocationModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 25.1125, longitude: 121.4582),
-        span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008))
+        center: CLLocationCoordinate2D(latitude: 25.144274, longitude: 121.381837),
+        span: MKCoordinateSpan(latitudeDelta: 0.08, longitudeDelta: 0.08))
 
     let locationManager = CLLocationManager()
 
